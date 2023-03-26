8f1b3270223d43e941b4df8d2ea033f2c209f972
aperezr20 <a.perezr20@uniandes.edu.co>
2023-03-26 13:38:03 -0500

    include MLP and KNN to grid search

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,48 +1,67 @@
-import sys
-import pandas as pd
-
-import sqlite3
-import pickle
-
-
-from sklearn.pipeline import Pipeline
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.multioutput import MultiOutputClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
-
 import os
+import sys
+import pickle
+import sqlite3
+import multiprocessing
 
+import pandas as pd
 import nltk
+
 from nltk.tokenize import word_tokenize
 from nltk.stem.wordnet import WordNetLemmatizer
+
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.metrics import classification_report, make_scorer, f1_score
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+
 nltk.download('punkt')
 nltk.download('wordnet')
 
 
 def load_data(database_filepath):
+    """
+    Load and preprocess data from a SQLite database.
+
+    Args:
+    database_filepath (str): Filepath to SQLite database
+
+    Returns:
+    X (DataFrame): DataFrame containing the feature data
+    y (DataFrame): DataFrame containing the target data
+    categories (list): List of category names
+    """
     
     # establish a connection to the SQLite database
     conn = sqlite3.connect(database_filepath)
-    
-
-    #df = pd.read_sql_query("SELECT * FROM table", conn)
-
-    
-    # read the table into a pandas DataFrame
     df = pd.read_sql_query("SELECT * FROM table_disaster", conn)
-    
-    #df = pd.read_sql("SELECT * FROM Message", engine)
-    df = df.replace(2,1)
-    
+
+    # replace 2 with 1
+    df = df.replace(2, 1)
+
     categories = list(df.columns[5:])
-    X = df[['message']] 
+    X = df[['message']]
     y = df[categories]
-    
+
     return X, y, categories
 
+
 def tokenize(text):
+    """
+    Tokenize and preprocess text data.
+
+    Args:
+    text (str): Text to be tokenized and preprocessed
+
+    Returns:
+    clean_tokens (list): List of cleaned tokens
+    """
+
     # Tokenizer
     tokens = word_tokenize(text.lower())
     
@@ -60,28 +79,73 @@ def tokenize(text):
 
 
 def build_model():
+    """
+    Build a pipeline and perform a grid search over different models and
+    hyperparameters.
+
+    Returns:
+    cv (GridSearchCV): A grid search object with a pipeline
+    """
+    
+    # Classifiers for grid search
+    clf_models = {
+        'Random Forest': [RandomForestClassifier()],
+        'Perceptron': [MLPClassifier()],
+        'KNN': [KNeighborsClassifier()]
+    }
+
+    # Classifiers' parameters for grid search
+    clf_params = [
+        {
+            'clf__estimator': clf_models['Random Forest'],
+            'clf__estimator__n_estimators': [50, 100]
+        },
+        {
+            'clf__estimator': clf_models['Perceptron'],
+            'clf__estimator__activation': ['tanh', 'relu'],
+            'clf__estimator__hidden_layer_sizes':[10]
+        },
+        {
+            'clf__estimator': clf_models['KNN'],
+            'clf__estimator__n_neighbors': [3, 5]
+        }
+    ]
     
     pipeline = Pipeline([
         ('vect', CountVectorizer(tokenizer=tokenize)),
         ('tfidf', TfidfTransformer()),
         ('clf', MultiOutputClassifier(estimator=RandomForestClassifier()))
     ])
-    
-    parameters = {
-        'clf__estimator__n_estimators':[10,50,100],
-        'clf__estimator__criterion':['gini', 'entropy'],
-        'clf__estimator__max_depth':[None, 10, 20]
-    }
 
-    cv = GridSearchCV(pipeline, param_grid=parameters, verbose=5, n_jobs=-1)
-    
+    # F1 scorer initialization for grid search
+    scorer = make_scorer(f1_score, zero_division=0, average='macro')
+
+    cv = GridSearchCV(
+        pipeline,
+        cv=2,
+        param_grid=clf_params,
+        verbose=5,
+        n_jobs=-1,
+        scoring=scorer
+    )
+
     return cv
-    
-    
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
+    """Evaluate the performance of the text classification model and print a classification report.
     
+    Args:
+        model (object): The trained model.
+        X_test (DataFrame): The test data containing message data.
+        Y_test (DataFrame): The test data containing target data.
+        category_names (list): The list of category names for target data.
+
+    Returns:
+        str: Returns the classification report as a string.
+    
+    """
+
     y_pred = model.predict(X_test.values.flatten())
     
     report = classification_report(Y_test.astype(int), y_pred.astype(int), target_names=category_names)
@@ -91,12 +155,21 @@ def evaluate_model(model, X_test, Y_test, category_names):
     return report
     
 
-
 def save_model(model, model_filepath):
-    pickle.load(model, open(model_filepath, 'wb'))
+    """Save the trained model as a pickle file.
+    
+    Args:
+        model (object): The trained model object to be saved.
+        model_filepath (str): The file path where the model object should be saved.
+
+    """
+
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
+    """Main function to execute the pipeline."""
+
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
@@ -108,9 +181,6 @@ def main():
         
         print('Training model...')
         model.fit(X_train.values.flatten(), Y_train)
-        
-        print('Best Parameters')
-        print(model.best_params_)
         
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
