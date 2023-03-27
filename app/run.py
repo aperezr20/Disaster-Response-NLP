b39a96fc88704c9e8af5df2ca5f2dea6fca39303
aperezr20 <a.perezr20@uniandes.edu.co>
2023-03-26 20:34:52 -0500

    update paths

--- app/run.py
+++ app/run.py
@@ -8,8 +8,9 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+import joblib
 from sqlalchemy import create_engine
+import sqlite3
 
 
 app = Flask(__name__)
@@ -26,11 +27,10 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
-
+conn = sqlite3.connect('../data/DisasterResponse.db')
+df = pd.read_sql_query("SELECT * FROM table_disaster", conn)
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
